ad64a6d6a84a109bbc877aca78337b9321bb4c5b
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2021-11-29 21:58:00 +0200

    Завантажено практичне завдання

---
+++ ISMPrakt05/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace ISMPrakt05
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.taskOneTextBox = new System.Windows.Forms.TextBox();
+            this.taskTwoTextBox = new System.Windows.Forms.TextBox();
+            this.taskTreeTextBox = new System.Windows.Forms.TextBox();
+            this.taskFourTextBox = new System.Windows.Forms.TextBox();
+            this.taskFiveTextBox = new System.Windows.Forms.TextBox();
+            this.taskSixTextBox = new System.Windows.Forms.TextBox();
+            this.taskSevenTextBox = new System.Windows.Forms.TextBox();
+            this.CalculateResultsButton = new System.Windows.Forms.Button();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.NnumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NnumericUpDown
+            // 
+            this.NnumericUpDown.Location = new System.Drawing.Point(52, 12);
+            this.NnumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NnumericUpDown.Name = "NnumericUpDown";
+            this.NnumericUpDown.Size = new System.Drawing.Size(63, 27);
+            this.NnumericUpDown.TabIndex = 0;
+            this.NnumericUpDown.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "N =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "суму від’ємних елементів масиву";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(340, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "максимальний серед парних елементів масиву";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(368, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "добуток елементів масиву, що мають парні індекси";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(350, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "номер (індекс) максимального елемента масиву";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(314, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "максимальний за модулем елемент масиву";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 227);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(242, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "суму індексів додатних елементів";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(268, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "кількість непарних елементів масиву";
+            // 
+            // taskOneTextBox
+            // 
+            this.taskOneTextBox.Location = new System.Drawing.Point(386, 54);
+            this.taskOneTextBox.Name = "taskOneTextBox";
+            this.taskOneTextBox.Size = new System.Drawing.Size(125, 27);
+            this.taskOneTextBox.TabIndex = 9;
+            // 
+            // taskTwoTextBox
+            // 
+            this.taskTwoTextBox.Location = new System.Drawing.Point(386, 87);
+            this.taskTwoTextBox.Name = "taskTwoTextBox";
+            this.taskTwoTextBox.Size = new System.Drawing.Size(125, 27);
+            this.taskTwoTextBox.TabIndex = 10;
+            // 
+            // taskTreeTextBox
+            // 
+            this.taskTreeTextBox.Location = new System.Drawing.Point(386, 125);
+            this.taskTreeTextBox.Name = "taskTreeTextBox";
+            this.taskTreeTextBox.Size = new System.Drawing.Size(125, 27);
+            this.taskTreeTextBox.TabIndex = 11;
+            // 
+            // taskFourTextBox
+            // 
+            this.taskFourTextBox.Location = new System.Drawing.Point(386, 158);
+            this.taskFourTextBox.Name = "taskFourTextBox";
+            this.taskFourTextBox.Size = new System.Drawing.Size(125, 27);
+            this.taskFourTextBox.TabIndex = 12;
+            // 
+            // taskFiveTextBox
+            // 
+            this.taskFiveTextBox.Location = new System.Drawing.Point(386, 191);
+            this.taskFiveTextBox.Name = "taskFiveTextBox";
+            this.taskFiveTextBox.Size = new System.Drawing.Size(125, 27);
+            this.taskFiveTextBox.TabIndex = 13;
+            // 
+            // taskSixTextBox
+            // 
+            this.taskSixTextBox.Location = new System.Drawing.Point(386, 224);
+            this.taskSixTextBox.Name = "taskSixTextBox";
+            this.taskSixTextBox.Size = new System.Drawing.Size(125, 27);
+            this.taskSixTextBox.TabIndex = 14;
+            // 
+            // taskSevenTextBox
+            // 
+            this.taskSevenTextBox.Location = new System.Drawing.Point(386, 257);
+            this.taskSevenTextBox.Name = "taskSevenTextBox";
+            this.taskSevenTextBox.Size = new System.Drawing.Size(125, 27);
+            this.taskSevenTextBox.TabIndex = 15;
+            // 
+            // CalculateResultsButton
+            // 
+            this.CalculateResultsButton.Location = new System.Drawing.Point(161, 12);
+            this.CalculateResultsButton.Name = "CalculateResultsButton";
+            this.CalculateResultsButton.Size = new System.Drawing.Size(119, 29);
+            this.CalculateResultsButton.TabIndex = 16;
+            this.CalculateResultsButton.Text = "Розрахувати";
+            this.CalculateResultsButton.UseVisualStyleBackColor = true;
+            this.CalculateResultsButton.Click += new System.EventHandler(this.CalculateResultsButton_Click);
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(554, 23);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(234, 415);
+            this.OutputTextBox.TabIndex = 17;
+            this.OutputTextBox.TextChanged += new System.EventHandler(this.OutputTextBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.CalculateResultsButton);
+            this.Controls.Add(this.taskSevenTextBox);
+            this.Controls.Add(this.taskSixTextBox);
+            this.Controls.Add(this.taskFiveTextBox);
+            this.Controls.Add(this.taskFourTextBox);
+            this.Controls.Add(this.taskTreeTextBox);
+            this.Controls.Add(this.taskTwoTextBox);
+            this.Controls.Add(this.taskOneTextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NnumericUpDown);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.NnumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown NnumericUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox taskOneTextBox;
+        private System.Windows.Forms.TextBox taskTwoTextBox;
+        private System.Windows.Forms.TextBox taskTreeTextBox;
+        private System.Windows.Forms.TextBox taskFourTextBox;
+        private System.Windows.Forms.TextBox taskFiveTextBox;
+        private System.Windows.Forms.TextBox taskSixTextBox;
+        private System.Windows.Forms.TextBox taskSevenTextBox;
+        private System.Windows.Forms.Button CalculateResultsButton;
+        private System.Windows.Forms.TextBox OutputTextBox;
+    }
+}
+
